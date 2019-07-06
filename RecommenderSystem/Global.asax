c6f54eced72b4c8f2153220e6b8712f256ba9e86
Kristina Antić <kristina.antic.II4@live.com>
2019-07-06 11:49:44 +0200

    importing project

---
+++ RecommenderSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecommenderSystem.MvcApplication" Language="C#" %>
---
+++ RecommenderSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecommenderSystem.Startup))]
+namespace RecommenderSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
