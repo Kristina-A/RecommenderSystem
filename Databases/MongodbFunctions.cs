8ae6b9b48c9ec4f206a714fc42732f554c1357a1
Kristina Antić <kristina.antic.II4@live.com>
2019-07-06 13:36:09 +0200

    connecting to databases

--- Databases/MongodbFunctions.cs
+++ Databases/MongodbFunctions.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using Databases.DomainModel;
 
 namespace Databases
 {
     public class MongodbFunctions
     {
+        MongoClient client;
+        IMongoDatabase db;
+
+        public MongodbFunctions()
+        {
+
+            client = new MongoClient("mongodb://localhost:27017");
+            db = client.GetDatabase("webshopdb");
+
+        }
     }
 }
--- Databases/TimescaledbFunctions.cs
+++ Databases/TimescaledbFunctions.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
 
 namespace Databases
 {
     public class TimescaledbFunctions
     {
+        NpgsqlConnection conn;
+        NpgsqlDataAdapter da;
+        DataSet ds;
+        DataTable dt;
+
+        public TimescaledbFunctions()
+        {
+            ds = new DataSet();
+            dt = new DataTable();
+            string connstring = String.Format("Server={0};Port={1};" +
+                    "User Id={2};Password={3};Database={4};",
+                    "127.0.0.1", "5432", "",
+                    "diplomski", "webshop");
+            conn = new NpgsqlConnection(connstring);
+            conn.Open();
+        }
     }
 }
