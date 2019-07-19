dbac5829f25d7b051d662713e4cf54673be519e4
miss_engineer <kristina.antic.ii4@live.com>
2019-07-19 13:35:54 +0200

    cosine comparer

---
+++ RecommendationEngine/CosineComparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecommendationEngine.Interfaces;
+
+namespace RecommendationEngine
+{
+    public class CosineComparer : IComparer
+    {
+        public double CompareVectors(double[] vectorOne, double[] vectorTwo)
+        {
+            double sumProduct = 0.0;
+            double sumOneSquared = 0.0;
+            double sumTwoSquared = 0.0;
+
+            for (int i = 0; i < vectorOne.Length; i++)
+            {
+                sumProduct += vectorOne[i] * vectorTwo[i];
+                sumOneSquared += Math.Pow(vectorOne[i], 2);
+                sumTwoSquared += Math.Pow(vectorTwo[i], 2);
+            }
+
+            return sumProduct / (Math.Sqrt(sumOneSquared) * Math.Sqrt(sumTwoSquared));
+        }
+    }
+}
---
+++ RecommendationEngine/LinearRater.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecommendationEngine.Interfaces;
+using RecommendationEngine.Objects;
+
+namespace RecommendationEngine
+{
+    public class LinearRater : IRater
+    {
+        public double GetRating(List<UserAction> actions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
